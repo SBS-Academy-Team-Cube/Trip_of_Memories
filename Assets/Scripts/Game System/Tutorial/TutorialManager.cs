ba5351d2a887b09a217daf4847b6fceb1aa418de
LJRight <binary_right@naver.com>
2026-03-25 16:52:52 +0900

    Add Tutorial

--- Assets/Scripts/Game System/Tutorial/TutorialManager.cs
+++ Assets/Scripts/Game System/Tutorial/TutorialManager.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using TMPro;
 using System.Collections.Generic;
 using System.Collections;
+using UnityEngine.InputSystem;
 
 public class TutorialManager : MonoBehaviour
 {
@@ -9,28 +10,48 @@ public class TutorialManager : MonoBehaviour
     [SerializeField] private float Duration = 10.0f;
     [SerializeField] private GameObject TextPanel;
     [SerializeField] private TMP_Text Text;
+    [SerializeField] private InputActionReference SkipAction;
+    [SerializeField] private InputActionReference PlayerJumpAction;
+    private Coroutine CurrentRoutine = null;
 
     public void ShowTutorialText(int Index)
     {
+        PlayerJumpAction.action.Disable();
+        SkipAction.action.Enable();
+        
         Text.SetText(Texts.GetText(Index));
         TextPanel.SetActive(true);
-        StartCoroutine(VisibleDuration());
+        CurrentRoutine = StartCoroutine(VisibleDuration());
     }
     private IEnumerator VisibleDuration()
     {
         yield return new WaitForSeconds(Duration);
+        PlayerJumpAction.action.Enable();
+        SkipAction.action.Disable();
         TextPanel.SetActive(false);
     }
 
     private void OnEnable() 
     {
         TutorialTriggerZone.OnTriggered += ShowTutorialText;
+        SkipAction.action.performed += OnSkip;
     }
     private void OnDisable() 
     {
         TutorialTriggerZone.OnTriggered -= ShowTutorialText;
+        SkipAction.action.performed -= OnSkip;
     }
 
+    void OnSkip(InputAction.CallbackContext Context)
+    {
+        if(CurrentRoutine != null)
+        {
+            StopCoroutine(CurrentRoutine);
+            TextPanel.SetActive(false);
+            PlayerJumpAction.action.Enable();
+            SkipAction.action.Disable();
+        }
+    }
 }
 
 [CreateAssetMenu(fileName = "TutorialText", menuName = "TutorialText", order = 0)]
