4e8d8547ee3d4501710bcab29d0f80675e28f33d
wonny0801 <wunha0530@gmail.com>
2026-04-08 16:56:01 +0900

    0408

--- Assets/Scripts/MiniGames/Pentomino/PentominoGameManager.cs
+++ Assets/Scripts/MiniGames/Pentomino/PentominoGameManager.cs
@@ -2,6 +2,16 @@ using UnityEngine;
 
 public class PentominoGameManager : MonoBehaviour
 {
-
-
+    private void OnEnable()
+    {
+        EventBus.PentominoClear += GameClear;
+    }
+    private void OnDisable()
+    {
+        EventBus.PentominoClear -= GameClear;
+    }
+    private void GameClear()
+    {
+        Debug.Log("Game Clear");
+    }
 }
