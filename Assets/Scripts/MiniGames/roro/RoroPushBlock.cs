22d971fa3655ddf891e83a06cf15c456783ca723
wonny0801 <wunha0530@gmail.com>
2026-04-10 15:25:22 +0900

    roro

--- Assets/Scripts/MiniGames/roro/RoroPushBlock.cs
+++ Assets/Scripts/MiniGames/roro/RoroPushBlock.cs
@@ -2,10 +2,13 @@ using UnityEngine;
 
 public class RoroPushBlock : MonoBehaviour
 {
-    // If the player pushes this block, it moves one tile in that direction together with the player.
-    // When the player performs a move action, if there is a pushable block in front, call the push function.
-    public void BlockMove(Vector3 MoveDir)
+    // Called when the block's parent needs to be changed in the hierarchy
+    public void PushBlockWithPlayerAsParent(GameObject player)
     {
-        // If this function is called, the object is pushed one tile in the moveDir direction (if no obstacle in front).
+        transform.SetParent(player.transform);
+    }
+    public void DetachBlockFromPlayer()
+    {
+        transform.SetParent(null);
     }
 }
