ea47e502d0ef75117e6ae7ee8da066c2e1d210d5
wonny0801 <wunha0530@gmail.com>
2026-04-29 17:00:37 +0900

    minigame scripting

--- Assets/Scripts/MiniGames/PathConection/PathPuzzleMng.cs
+++ Assets/Scripts/MiniGames/PathConection/PathPuzzleMng.cs
@@ -2,15 +2,40 @@ using UnityEngine;
 
 public class PathPuzzleMng : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Puzzle Answer")]
+    [SerializeField] private int greenAnswer = 0;
+    [SerializeField] private int blueAnswer = 0;
+    [SerializeField] private int redAnswer = 0;
+
+    [SerializeField] private PathInputHandler inputHandler;
+
+    private void Awake()
     {
-        
+        if (inputHandler == null)
+            Debug.LogError("inputhandler is null");
+    }
+    private void OnEnable()
+    {
+        inputHandler.ClearCheckEvent.AddListener(ClearCheck);
+    }
+    private void OnDisable()
+    {
+        inputHandler.ClearCheckEvent.RemoveListener(ClearCheck);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void ClearCheck()
     {
-        
+        if(greenAnswer == (inputHandler.GreenRollCount % 4) &&
+            blueAnswer == (inputHandler.BlueRollCount % 4) &&
+            redAnswer == (inputHandler.RedRollCount % 4))
+        {
+            Clear();
+        }
     }
+
+    private void Clear()
+    {
+        Debug.Log("Clear");
+    }
+
 }
