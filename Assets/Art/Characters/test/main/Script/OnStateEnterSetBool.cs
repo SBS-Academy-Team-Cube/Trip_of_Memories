b92270605118d3871532f8c93a9135242914ace9
wonny0801 <wunha0530@gmail.com>
2026-05-05 18:08:19 +0900

    animator

---
+++ Assets/Art/Characters/test/main/Script/OnStateEnterSetBool.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class OnStateEnterSetBool : StateMachineBehaviour
+{
+    public string boolName = "MoveLock";
+    public bool value = true;
+
+
+
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.SetBool(boolName, value);
+    }
+
+}
