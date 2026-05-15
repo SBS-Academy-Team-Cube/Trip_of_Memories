f9fb2f71df98fba4861bcb19ce3b6c6271bad5f7
wonny0801 <wunha0530@gmail.com>
2026-05-15 16:17:23 +0900

    .

--- Assets/Scripts/MiniGames/SlidePuzzle/SlidePuzzleMng.cs
+++ Assets/Scripts/MiniGames/SlidePuzzle/SlidePuzzleMng.cs
@@ -2,5 +2,38 @@ using UnityEngine;
 
 public class SlidePuzzleMng : MonoBehaviour
 {
-    
+    [SerializeField] private SlidePuzzleUIMng UIMng;
+    [SerializeField] private SlidePuzzleInputHandler InputHandler;
+    [SerializeField] private SlidePuzzleBoard board;
+
+    private void Start() // testcode
+    {
+        GameStart();
+    }
+
+
+
+    public void GameStart()
+    {
+        if (InputHandler == null)
+            return;
+        if (board == null)
+            return;
+        if (UIMng == null)
+            return;
+
+
+        InputHandler.GameInit();
+        board.GameInit();
+        UIMng.GameInit();
+        UIMng.GameStart();
+    }
+    public void GameReset()
+    {
+        UIMng.GameInit();
+    }
+    public void GameClear()
+    {
+        UIMng.GameClear();
+    }
 }
